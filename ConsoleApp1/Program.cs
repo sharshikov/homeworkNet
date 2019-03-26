c5d386f831d457e7eda494ac1e310d84929ea819
user <dsdd>
2019-03-27 00:01:03 +0300

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,28 +1,21 @@
 ﻿using Bogus;
+using Bogus.DataSets;
 using System;
+using System.Text;
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var modelFaker = new Faker<MyModel>("en").
-             RuleFor(o => o.Name, f => f.Name.FirstName()).
-             RuleFor(o => o.SurName, f => f.Name.LastName()).
-             RuleFor(o => o.Born, f => f.Date.Past(34,new DateTime(1990,12,4)));
+            var faker = new Faker("ru");
             for (int i = 0; i < 10000; i++)
             {
-                var myModel = modelFaker.Generate();
-                Console.WriteLine(myModel.Name + " " + myModel.SurName + " " + myModel.Born.ToShortDateString());
+                Console.WriteLine(faker.Name.FullName()+" "+ faker.
+                    Date.Past(32, new DateTime(2000, 12, 12)).ToShortDateString());
             }
             Console.ReadKey();
         }
-    }
-
-    class MyModel
-    {
-        public string Name { get; set; }
-        public string SurName { get; set; }
-        public DateTime Born { get; set; }
     }
 }
